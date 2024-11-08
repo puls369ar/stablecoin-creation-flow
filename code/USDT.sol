913fdb467305415c84b4c2598558f378899d55f1
puls369ar <155809127+puls369ar@users.noreply.github.com>
2024-11-08 20:46:27 +0400

    Update USDT.sol

--- code/USDT.sol
+++ code/USDT.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT LICENSE
 
+
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract USDT is ERC20, ERC20Burnable, Ownable {
+
+  using SafeERC20 for ERC20;
+
+  constructor() Ownable(_msgSender()) ERC20("Tether USD", "USDT") {}
+
+  function mint(uint256 amount) external onlyOwner {
+    _mint(msg.sender, amount);
+  }
+
+}
